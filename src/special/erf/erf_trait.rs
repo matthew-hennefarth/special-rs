e9f066d9d7f5c13481b53263eece5b949443bdf3
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-08-10 22:23:07 -0500

    added documentation for erf and erfc

--- src/special/erf/erf_trait.rs
+++ src/special/erf/erf_trait.rs
@@ -13,11 +13,50 @@ pub trait Erf {
     /// $$
     /// It is an entire complex function with no singularities (except at infinity). It is also called the Gauss error function. For a more thorough explanation of the error function and all of its properties, it is recommended to read through the [DLMF] and [wiki] pages.
     ///
+    /// # Examples
+    /// For real-valued inputs
+    /// ```
+    /// use sci_rs::special::Erf;
+    /// assert!((2.0_f32.erf() - 0.9953222650189527341).abs() < 1e-14);
+    /// assert_eq!((-3.2).erf(), -(3.2.erf())); // Error Function is odd!
+    /// ```
+    /// # Notes
+    /// The real-valued implementation is based on the [Boost] library version (v 1.82.0) ([documentation](https://www.boost.org/doc/libs/1_82_0/libs/math/doc/html/math_toolkit/sf_erf/error_function.html)). Largely, it uses various rational polynomial approximations. Additionally, it uses the fact that the Error function is odd to always evaluate at positive arguments. At times, it will use the [Compliment Error function] to evaluate.
+    ///
+    /// # References
+    /// - [Boost Error function implementation][Boost] version 1.82.0
+    /// - [DLMF]
+    /// - [Wikipedia][wiki]
+    ///
     /// [comment]: <> (Reference hyperlinks)
+    /// [Boost]: https://www.boost.org/doc/libs/1_82_0/boost/math/special_functions/erf.hpp
     /// [DLMF]: https://dlmf.nist.gov/7.2
     /// [wiki]: https://en.wikipedia.org/wiki/Error_function
+    /// [Compliment Error function]: crate::special::erf::Erf::erfc
     fn erf(self) -> Self;
 
+    /// The Complementary Error Function.
+    /// $$
+    /// \mathrm{erfc}z = 1 - \mathrm{erf}z
+    /// $$
+    /// See the [wiki] and [DLMF] pages for a more detailed explanation of the [Error function] and its properties.
+    ///
+    /// # Examples
+    /// For real-valued inputs
+    /// ```
+    /// use sci_rs::special::Erf;
+    /// assert!((2.0_f32.erfc() - 0.004677734981047265837).abs() < 1e-7);
+    /// assert_eq!((-3.2).erfc(), 1.0 + (3.2.erf())); // Error and Complementary are related
+    /// assert_eq!(3.0.erfc() + 3.0.erf(), 1.0);
+    /// ```
+    /// # Notes
+    /// The real-valued implementation is based on the [Boost] library (v 1.82.0) ([documentation](https://www.boost.org/doc/libs/1_82_0/libs/math/doc/html/math_toolkit/sf_erf/error_function.html)).
+    ///
+    /// [comment]: <> (Reference hyperlinks)
+    /// [Boost]: https://www.boost.org/doc/libs/1_82_0/boost/math/special_functions/erf.hpp
+    /// [wiki]: https://en.wikipedia.org/wiki/Error_function
+    /// [DLMF]: https://dlmf.nist.gov/7.2
+    /// [Error function]: crate::special::erf::Erf::erf
     fn erfc(self) -> Self;
 }
 
